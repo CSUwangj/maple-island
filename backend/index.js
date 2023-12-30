18cf2d3500d9068daab3d5bd17b89919b18d7d40
CSUwangj <CSUwangj@protonmail.com>
2023-12-30 22:28:21 +0800

    feat(backend): allow cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const character = require('./api/character')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json({ extended: false }))
-
 app.use('/character', character)
 
 const PORT = process.env.PORT || 8000
