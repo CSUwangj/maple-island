48a6d4ad902294ee29e91a9dff51797678598d9d
CSUwangj <CSUwangj@protonmail.com>
2024-01-01 05:19:12 +0800

    feat(netlify): make netlify happy

--- backend/netlify/functions/api.ts
+++ backend/netlify/functions/api.ts
@@ -1,12 +1,19 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
 const router = express.Router()
 const axios = require('axios')
+import serverless from "serverless-http"
 
-router.get('/info/:regoin/:name', async (req, res) => {
+app.use(cors())
+app.use(express.json({ extended: false }))
+
+router.get('/character/info/:regoin/:name', async (req, res) => {
   const r = await axios(`https://api.maplestory.gg/v2/public/character/${req.params.region}/${req.params.name}`)
   const img = await axios(`${r.data.CharacterData.CharacterImageURL}`, { responseType: 'arraybuffer' })
   r.data.CharacterData.Image = Buffer.from(img.data, 'binary').toString('base64')
   res.send(r.data)
 })
+app.use('/api/', router)
 
-module.exports = router
+export const handler = serverless(app)
