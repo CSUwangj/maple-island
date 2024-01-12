09b0271d1fc3fa0125b9f245aebc27833a76ae05
CSUwangj <CSUwangj@protonmail.com>
2024-01-13 04:22:37 +0800

    feat(composer): dnd support

--- frontend/src/route.tsx
+++ frontend/src/route.tsx
@@ -4,6 +4,7 @@ import { Route, Routes as ReactRoutes } from 'react-router-dom'
 import { Page as Index } from 'page/index'
 import { Page as Legion } from 'page/legion'
 import { Page as Solver } from "page/legion/solver"
+import { Page as Composer } from 'page/legion/composer'
 import { Page as MidnightChaserHelper } from 'page/helpers/midnight-chaser'
 import { Page as HungryMutoHelper } from 'page/helpers/hungry-muto'
 import { Page as EsferaGuardianHelper } from 'page/helpers/esfera-guardian'
@@ -14,6 +15,7 @@ export const Routes: React.FC = () => {
       <Route path='/' element=<Index />/>
       <Route path='/legion'  element=<Legion />/>
       <Route path='/legion/solver' element=<Solver />/>
+      <Route path='/legion/composer' element=<Composer />/>
       <Route path='/helper/midnight-chaser' element=<MidnightChaserHelper />/>
       <Route path='/helper/hungry-muto' element=<HungryMutoHelper />/>
       <Route path='/helper/esfera-guardian' element=<EsferaGuardianHelper />/>
